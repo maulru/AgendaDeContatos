075297381603536e20f22de927c9c70af440606b
Antonio Kauã <kauabatista545@hotmail.com>
2024-09-04 19:11:07 -0300

    - Adicionado datasource e dashboards do Grafana - Adicionado o arquivo de configuração do prometheus - Criação do DCProj para compatibilidade com o VS. - Alteração da connection string para direcionar corretamente para o banco de dados instanciado no container

--- WebApp/MeusContatos_Testes/ContatoControllerTestBase.cs
+++ WebApp/MeusContatos_Testes/ContatoControllerTestBase.cs
@@ -2,6 +2,6 @@
 {
     public class ContatoControllerTestBase
     {
-        public readonly string _connectionString = "Server=localhost,1433;Database=contatos;User id=sa;Password=M@sterk3y;TrustServerCertificate=True";
+        public readonly string _connectionString = "Server=sqlserver;Database=contatos;User id=sa;Password=M@sterk3y;TrustServerCertificate=True";
     }
 }
