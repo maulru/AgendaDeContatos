ac00a8573905ae7bb58106eac629e63420e1c402
Mauro Alves Jr <31009363+maulru@users.noreply.github.com>
2024-10-01 01:52:26 -0300

    Implementada exclusão de contatos via fila

---
+++ API/ExcluiContatoAPI/ExcluiContatoAPI/Controllers/ExcluiContatoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prometheus;
+using Core.Repository;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExcluiContatoAPI.Controllers
+{
+    public class ExcluiContatoController : Controller
+    {
+
+        #region Propriedades
+        private readonly IContatoRepository _contatoRepository;
+        private Counter contatosExcluidos = Metrics.CreateCounter("contatos_excluidos", "Contatos Excluidos");
+        private Counter homeExibicoes = Metrics.CreateCounter("home_exibicoes", "Exibições da lista de contatos");
+        private static readonly Histogram RequestDurationHome = Metrics
+            .CreateHistogram("request_duration_home", "Duração das requisições para a página inicial em segundos",
+             new HistogramConfiguration
+             {
+                 Buckets = Histogram.LinearBuckets(start: 0.1, width: 0.1, count: 10)
+             });
+        private static readonly Histogram RequestDurationExcluir = Metrics
+            .CreateHistogram("request_duration_excluir", "Duração das requisições para exclusão de contatos em segundos",
+             new HistogramConfiguration
+             {
+                 Buckets = Histogram.LinearBuckets(start: 0.1, width: 0.1, count: 10)
+             });
+
+        #endregion
+
+
+        public ExcluiContatoController(IContatoRepository contatoRepository)
+        {
+            _contatoRepository = contatoRepository;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public JsonResult ExcluirContato(int id)
+        {
+            using (RequestDurationExcluir.NewTimer())
+            {
+                try
+                {
+                    _contatoRepository.Deletar(id);
+                    contatosExcluidos.Inc();
+                    return Json(new { success = true });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false, message = ex.Message });
+                }
+            }
+        }
+
+    }
+}
---
+++ API/ExcluiContatoAPI/ExcluiContatoAPI/Program.cs
@@ -0,0 +1,51 @@
+using Core.Repository;
+using Infrastructure;
+using Infrastructure.Repository;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("ConnectionString"));
+}, ServiceLifetime.Scoped);
+
+builder.Services.AddScoped<ExcluiService>();
+
+builder.Services.AddScoped<IContatoRepository, ContatoRepository>();
+
+builder.Services.AddScoped<ITelefoneRepository, TelefoneRepository>();
+
+builder.Services.AddHostedService<RabbitMQExcluiConsumerService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
