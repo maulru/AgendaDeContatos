d13fe3a17b66ea9f19effef184ac7d8de8d9676e
Antonio Kauã <kauabatista545@hotmail.com>
2024-10-03 00:33:25 -0300

    Alteração para suporte ao Rabbit MQ e ajuste na pipeline

--- API/AdicionarContatoAPI/AdicionarContatoAPI/Consumer/RabbitMQAddPhoneConsumer.cs
+++ API/AdicionarContatoAPI/AdicionarContatoAPI/Consumer/RabbitMQAddPhoneConsumer.cs
@@ -23,7 +23,7 @@ namespace AdicionarContatoAPI.Consumer
         public RabbitMQAddPhoneConsumer(IContatoRepository contatoRepository, ITelefoneRepository telefoneRepository,
             IServiceScopeFactory serviceScopeFactory)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
             _connection = factory.CreateConnection();
             _channel = _connection.CreateModel();
             _channel.QueueDeclare(queue: queueName,
